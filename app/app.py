4f7a31ae66558be48e827919bb978ff8ea48ad7e
Francis Sulkowski <sulkowskif@yahoo.com>
2024-12-09 05:44:46 -0500

    Heroku Live

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, render_template
 import pandas as pd
 
+merged_df = pd.read_csv('app/Resources/merged_data.csv')
 
 app = Flask(__name__)
 
@@ -19,32 +20,32 @@ def prop3():
     return render_template('visualizations.html')
 
 
-    @app.route('/heatmap')
-    def prop4():
-        # Convert the DataFrame to JSON
-        data_json = merged_df.to_json(orient='records')
-        print(data_json)  # Log the data for debugging
+@app.route('/heatmap')
+def prop4():
+    # Convert the DataFrame to JSON
+    data_json = merged_df.to_json(orient='records')
+    print(data_json)  # Log the data for debugging
 
-        # Get unique years for the dropdown
-        unique_years = merged_df['report_year'].unique()
+    # Get unique years for the dropdown
+    unique_years = merged_df['report_year'].unique()
 
-        return render_template('heatmap.html', data=data_json, years=unique_years)
+    return render_template('heatmap.html', data=data_json, years=unique_years)
 
-    @app.route('/update_map/<int:year>')
-    def update_map(year):
-        # Prepare data for the heatmap for the selected year
-        filtered_data = merged_df[merged_df['report_year'] == year]
-        heat_data = [[row['Latitude'], row['Longitude'], row['crimes_percapita']] 
-                    for index, row in filtered_data.iterrows()]
+@app.route('/update_map/<int:year>')
+def update_map(year):
+    # Prepare data for the heatmap for the selected year
+    filtered_data = merged_df[merged_df['report_year'] == year]
+    heat_data = [[row['Latitude'], row['Longitude'], row['crimes_percapita']] 
+                  for index, row in filtered_data.iterrows()]
 
-        # Return the heat data as JSON
-        return jsonify({'heat_data': heat_data})
+    # Return the heat data as JSON
+    return jsonify({'heat_data': heat_data})
 
-    @app.route('/view_map/<int:year>')
-    def view_map(year):
-        # Serve the generated heatmap HTML file
-        map_file = f'crime_heatmap_{year}.html'
-        return render_template('view_map.html', map_file=map_file)
+@app.route('/view_map/<int:year>')
+def view_map(year):
+    # Serve the generated heatmap HTML file
+    map_file = f'crime_heatmap_{year}.html'
+    return render_template('view_map.html', map_file=map_file)
 
 
 @app.route("/safest_cities")
