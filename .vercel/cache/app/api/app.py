e7d6182773a2521e0ef7d0b72cbc98bb3c5fd124
Francis Sulkowski <sulkowskif@yahoo.com>
2024-12-09 02:35:07 -0500

    Vercel Test

--- .vercel/cache/app/api/app.py
+++ .vercel/cache/app/api/app.py
@@ -7,11 +7,7 @@ import os
 
 merged_df = pd.read_csv("Resources/merged_data.csv")
 
-if os.path.exists("Resources/merged_data.csv"):
-    merged_df = pd.read_csv("Resources/merged_data.csv")
-else:
-    print(f"Error: File '{"Resources/merged_data.csv"}' not found.")
-    # Handle the error as needed, maybe set merged_df to None or provide a default dataset
+
 
 app = Flask(__name__, 
             static_folder='static', 
@@ -32,33 +28,6 @@ def prop2():
 def prop3():
     return render_template('visualizations.html')
 
-
-@app.route('/heatmap')
-def prop4():
-    # Convert the DataFrame to JSON
-    data_json = merged_df.to_json(orient='records')
-    print(data_json)  # Log the data for debugging
-
-    # Get unique years for the dropdown
-    unique_years = merged_df['report_year'].unique()
-
-    return render_template('heatmap.html', data=data_json, years=unique_years)
-
-@app.route('/update_map/<int:year>')
-def update_map(year):
-    # Prepare data for the heatmap for the selected year
-    filtered_data = merged_df[merged_df['report_year'] == year]
-    heat_data = [[row['Latitude'], row['Longitude'], row['crimes_percapita']] 
-                  for index, row in filtered_data.iterrows()]
-
-    # Return the heat data as JSON
-    return jsonify({'heat_data': heat_data})
-
-@app.route('/view_map/<int:year>')
-def view_map(year):
-    # Serve the generated heatmap HTML file
-    map_file = f'crime_heatmap_{year}.html'
-    return render_template('view_map.html', map_file=map_file)
 
 
 @app.route("/safest_cities")
