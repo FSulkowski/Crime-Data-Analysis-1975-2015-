4aa6161514dfbaafb1809dc40f5b43d1eb51d233
Francis Sulkowski <sulkowskif@yahoo.com>
2024-12-09 03:59:54 -0500

    Heroku Test

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, render_template
 import pandas as pd
+import os
 
 
 app = Flask(__name__)
@@ -39,3 +40,4 @@ def prop8():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
